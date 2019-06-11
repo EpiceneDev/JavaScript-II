0ead95f5e8eda4260b8b69ae352664e2fd50b1de
Lambda_School_Loaner_125 <lambda_school_loaner_125@Lambda-School-Loaner-125s-MacBook-Pro.local>
2019-06-11 15:42:11 -0700

    MVP met

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+  let count = 0;
+  function countUp() {
+    count++
+  }
+  countUp()
+  console.log(count)
+  countUp()
+  console.log(count)
+  countUp()
+  console.log(count)
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
